bd424c81869cef40e17c905f890df02864f0a16f
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2025-04-05 17:37:33 +0200

    Added an alias in the main file

--- src/parsing-lib.cr
+++ src/parsing-lib.cr
@@ -2,17 +2,19 @@ require "./parser"
 
 source = {{ read_file "./examples/example1.alta" }}
 
-p = Parser.char '|'
+alias P = Parser
+
+p = P.char '|'
 puts p.parse source
 
-p2 = Parser.string "|s"
+p2 = P.string "|s"
 puts p2.parse source
 
-p3 = Parser.many(Parser.digit).fby(Parser.char('a').or Parser.char 'b').map{|tup| tup[0] << tup[1]}
+p3 = P.many(P.digit).fby(P.char('a').or P.char 'b').map{|tup| tup[0] << tup[1]}
 puts p3.parse "123bbc"
 
-p4 = Parser.not Parser.digit.fby(Parser.char('a'))
+p4 = P.not P.digit.fby(P.char('a'))
 puts p4.parse "1b23"
 
-p5 = (Parser.many Parser.not_in("|,:")).map{|l| l.join()}
+p5 = (P.many P.not_in("|,:")).map{|l| l.join()}
 puts p5.parse("abc123:")
